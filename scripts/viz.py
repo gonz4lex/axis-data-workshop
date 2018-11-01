d1b02fcd4e9ae04ff9cff00ef6a086705d8e2be7
gonz4lex <alejandrogcaules@gmail.com>
2018-11-01 17:53:32 +0100

    generate clustered data correctly at last

--- scripts/viz.py
+++ scripts/viz.py
@@ -9,6 +9,8 @@ import matplotlib.pyplot as plt
 import json
 import random
 
+pd.options.mode.chained_assignment = None
+
 with open('thrones.json') as f:
     data = json.load(f)
 
@@ -51,16 +53,25 @@ df = df[df['house'].isin(
                     
 df['gold'] = np.nan
 df['weapons'] = np.nan
+df['multiplier'] = np.nan
 
 random.seed(2018)
 
-for i in range(len(df)):
-    df['gold'][i] = random.gauss(200, 20)
-    df['weapons'][i] = random.gauss(100, 1)
+full = pd.DataFrame()
+
+for i in top_houses.index:
+    tmp = df[df['house'] == i]
+    tmp['gold_multiplier'] = abs(random.gauss(3, 6))
+    tmp['weapons_multiplier'] = abs(random.gauss(3, 6))
+    full = pd.concat([full, tmp], ignore_index = True)
+
+for i in range(len(full)):
+    full['gold'][i] = random.gauss(200, 50) * full['gold_multiplier'][i]
+    full['weapons'][i] = random.gauss(100, 20) * full['weapons_multiplier'][i]
 
 sns.pairplot(x_vars = 'weapons', 
              y_vars = 'gold', 
-             data = df, 
+             data = full, 
              hue = "house"
 )
 
